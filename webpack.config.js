ba901cfd73c0d81ac9acc329d0337343170e45bc
Bestian Tang 大巴 <bestian@gmail.com>
2024-09-21 01:31:55 +0800

    add favicon and og

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -24,7 +25,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' }
+      ],
+    }),
   ],
   devServer: {
     static: {
@@ -32,5 +38,6 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+    open: true,
   },
 };
