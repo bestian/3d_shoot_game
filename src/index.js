8e126d58c6859150d39c40cd29cd3c1ce9d6674e
Bestian Tang 大巴 <bestian@gmail.com>
2024-09-21 17:45:15 +0800

    close #6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-
 import { Monster } from './monster.js';
 import { Game } from './game.js';
+
+// 導入 CSS 文件
+import './css/ipad.css';
+import './css/iphone.css';
 
 // 初始化遊戲邏輯
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
