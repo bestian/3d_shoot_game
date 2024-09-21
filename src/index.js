1f7452b4c89b9fa59e7668ab9ab3361cddd20414
Bestian Tang 大巴 <bestian@gmail.com>
2024-09-21 17:53:39 +0800

    close #5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { Monster } from './monster.js';
 import { Game } from './game.js';
 
 // 導入 CSS 文件
-import './css/ipad.css';
 import './css/iphone.css';
+import './css/ipad.css';
+import './css/desktop.css';
 
 // 初始化遊戲邏輯
